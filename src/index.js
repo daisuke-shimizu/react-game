f469571d48ed21d174d1bdeb4d3c398a7f9ace20
daisuke-shimizu <vitamin.daisuke.0901@gmail.com>
2020-11-07 16:08:50 +0900

    made index.css

--- src/index.js
+++ src/index.js
@@ -6,15 +6,17 @@ class Square extends React.Component {
     render() {
       return (
         <button className="square">
-          {/* TODO */}
+          {this.props.value}
         </button>
       );
     }
   }
-  
+
+//   boardは親、squareは子。親から子にコンポーネントを渡す。
+
   class Board extends React.Component {
     renderSquare(i) {
-      return <Square />;
+      return <Square value={i} />;
     }
   
     render() {
