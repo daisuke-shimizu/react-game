cd35f8629eefef38f15664506d7823db06b03eaa
daisuke-shimizu <vitamin.daisuke.0901@gmail.com>
2020-11-07 18:48:34 +0900

    game done

--- src/index.js
+++ src/index.js
@@ -3,24 +3,37 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 // squareコンポーネントは1 つの <button> をレンダーし、Board（盤面）が 9 個のマス目をレンダーしています
-class Square extends React.Component {
-    // constructor(props) {
-    //     super(props);
-    //     this.state = {
-    //         value: null,
-    //     };
-    // }
-    render() {
-      return (
-        <button 
-            className="square" 
-            onClick={() => this.props.onClick()}
+// squareコンポーネントは,自分のstateを持たないい
+// class Square extends React.Component {
+//     // constructor(props) {
+//     //     super(props);
+//     //     this.state = {
+//     //         value: null,
+//     //     };
+//     // }
+//     render() {
+//       return (
+//         <button 
+//             className="square" 
+//             onClick={() => this.props.onClick()}
+//             >
+//           {this.props.value}
+//         </button>
+//       );
+//     }
+//   }
+
+
+// squareを関数コンポーネントに書き換える
+function Square(props) {
+    return (
+        <button className="square" 
+            onClick={props.onClick}
             >
-          {this.props.value}
+          {props.value}
         </button>
       );
-    }
-  }
+}
 
 //   boardは親、squareは子。親から子にコンポーネントを渡す。
 
@@ -29,13 +42,20 @@ class Square extends React.Component {
         super(props);
         this.state = {
             squares: Array(9).fill(null),
+            xIsNext: true,
         };
     }
 
     handleClick(i) {
         const squares = this.state.squares.slice();
-        squares[i] = 'X';
-        this.setState({squares: squares});
+        if (calculateWinner(squares) || squares[i]) {
+            return;
+        }
+        squares[i] = this.state.xIsNext ? 'X' : '0'
+        this.setState({
+            squares: squares,
+            xIsNext: !this.state.xIsNext,
+        });
       }
 
     renderSquare(i) {
@@ -48,7 +68,15 @@ class Square extends React.Component {
     }
   
     render() {
-      const status = 'Next player: X';
+    //   const status = 'Next player: '+
+    //   (this.state.xIsNext ? 'X' : '0');
+    const winner = calculateWinner(this.state.squares);
+    let status;
+    if (winner) {
+      status = 'Winner: ' + winner;
+    } else {
+      status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+    }
   
       return (
         <div>
@@ -96,3 +124,23 @@ class Square extends React.Component {
     document.getElementById('root')
   );
   
+
+  function calculateWinner(squares) {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];
+      if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+        return squares[a];
+      }
+    }
+    return null;
+  }
